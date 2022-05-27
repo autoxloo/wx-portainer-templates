7d88aab3036d5a4758d8fb50d6ec1e981e5d6d93
artem-levashov <artem.levashov@autoxloo.com>
2022-05-27 15:36:23 +0300

    update run rummand with name

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 docker build -t wx-portainer-templates . \
-&& docker run -d -p "8080:80" -v "${PWD}/templates.json:/usr/share/nginx/html/templates.json" -v "${PWD}/templates-2.0.json:/usr/share/nginx/html/templates-2.0.json" -v "${PWD}/templates-1.20.0.json:/usr/share/nginx/html/templates-1.20.0.json" wx-portainer-templates
+&& docker run --name portainer-templates -d -p "8080:80" -v "${PWD}/templates.json:/usr/share/nginx/html/templates.json" -v "${PWD}/templates-2.0.json:/usr/share/nginx/html/templates-2.0.json" -v "${PWD}/templates-1.20.0.json:/usr/share/nginx/html/templates-1.20.0.json" wx-portainer-templates
